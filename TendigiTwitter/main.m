5ec74db01ced2e7e2a550ea12e132d55a5ea81d8
Baconthorpe <zabuhoff@gmail.com>
2014-05-13 13:46:02 -0400

    Initial Commit

---
+++ TendigiTwitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TendigiTwitter
+//
+//  Created by Ezekiel Abuhoff on 5/13/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAAppDelegate class]));
+    }
+}
